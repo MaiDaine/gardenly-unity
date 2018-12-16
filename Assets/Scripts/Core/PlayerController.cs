8de2271fa75953d566ddeb6b5ec0d1d50ed5ce13
David Maidenberg <david.maidenberg@epitech.eu>
2018-12-16 11:34:51 +0100

    Fix default values for CameraController and add min/max altitude Create PlayerController and WallHandler with basic logic

---
+++ Assets/Scripts/Core/PlayerController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    enum ConstructionState { Off, Positioning, Building};
+
+    Camera viewCamera;
+    public WallHandler Ghost;
+    private ConstructionState currentState = ConstructionState.Off;
+
+    void Start()
+    {
+        viewCamera = Camera.main;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void LateUpdate()
+    {
+        if (currentState == ConstructionState.Off && Input.GetKey("p"))
+            SpawnGhost();
+
+        if (currentState != ConstructionState.Off)
+        {
+            Ray ray = viewCamera.ScreenPointToRay(Input.mousePosition);
+            Plane goundPlane = new Plane(Vector3.up, Vector3.zero);
+            float rayDistance;
+
+            if (goundPlane.Raycast(ray, out rayDistance))
+            {
+                Debug.DrawLine(ray.origin, ray.GetPoint(rayDistance), Color.red);
+
+                if (currentState == ConstructionState.Positioning)
+                    UpdateGhostPosition(ray.GetPoint(rayDistance));
+                if (currentState == ConstructionState.Positioning && Input.GetMouseButtonDown(0))
+                    StartConstruction(ray.GetPoint(rayDistance));
+            }
+        }
+    }
+
+    void SpawnGhost()
+    {
+        Ghost = Instantiate(Ghost, Vector3.zero, Quaternion.identity) as WallHandler;  
+        currentState = ConstructionState.Positioning;
+    }
+
+    void UpdateGhostPosition(Vector3 point)
+    {
+        Ghost.transform.position = point;
+    }
+
+    void StartConstruction(Vector3 point)
+    {
+
+    }
+}
---
+++ Assets/Scripts/StaticElements/WallHandler.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(MeshFilter))]
+[RequireComponent (typeof(MeshRenderer))]
+[RequireComponent (typeof(BoxCollider))]
+[RequireComponent (typeof(Material))]
+public class WallHandler : MonoBehaviour
+{
+    MeshFilter mesh;
+
+    void Start()
+    {
+        //tmp test
+        mesh = GetComponent<MeshFilter>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Move(Vector3 position)
+    {
+        
+    }
+}
