bafda691d7ef8e70f30284c5448212bef72e1249
yan.fasquelle@epitech.eu <yan.fasquelle@epitech.eu>
2018-12-24 16:44:41 +0200

    ToolBar for show/hide menu + prefab Menu/MenuButton + first model of preview window when a Dynamic object is selected

--- Assets/Scripts/Core/DynamicElemHandler.cs
+++ Assets/Scripts/Core/DynamicElemHandler.cs
@@ -1,10 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DynamicElemHandler : GhostHandler
 {
+    public  Transform windowPreview;
 
+    protected bool isSelected;
+    protected Transform previewUI;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,11 +18,22 @@ public class DynamicElemHandler : GhostHandler
     //ISELECTABLE
     public override void Select()
     {
-        // OPEN CANVAS WITH INFO + INTERACTION
+        Debug.Log("SELECT");
+        previewUI = Instantiate(windowPreview, Vector3.zero, Quaternion.identity);
+        previewUI.transform.position = transform.position;
+        previewUI.transform.SetParent(transform, false);
+        Transform panel = previewUI.GetChild(0);
+        
+        RectTransform rect = previewUI.GetComponent<RectTransform>();
+        rect.sizeDelta = new Vector2(40, 20);
+        panel.position = new Vector3(transform.position.x, transform.position.y + 20, transform.position.z);
+        isSelected = true;
     }
 
     public override void DeSelect()
     {
-        // CLOSE CANVAS WITH INFO + INTERACTION
+        Debug.Log("DESELECT");
+        isSelected = false;
+        Destroy(previewUI.gameObject);
     }
 }
---
+++ Assets/Scripts/UI/MenuScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuScript : MonoBehaviour
+{
+    public bool state = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.SetActive(state);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void    ChangeState()
+    {
+        gameObject.SetActive(!state);
+        state = !state;
+    }
+}
