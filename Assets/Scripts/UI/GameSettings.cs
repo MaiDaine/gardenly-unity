30e090dc3e4142bbe491064a41a3460fec0a4cda
yan.fasquelle@epitech.eu <yan.fasquelle@epitech.eu>
2019-03-09 16:07:09 +0200

    first version of settings menu with link ok

---
+++ Assets/Scripts/UI/GameSettings.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSettings : MonoBehaviour
+{
+    public bool fullscreen;
+    public Resolution resolution;
+    public bool music;
+    public float volume;
+    public int quality;
+    public int antialiasing;
+}
