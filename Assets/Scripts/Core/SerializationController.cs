9e16e4ae0c506406a1e008d562feba8cc8468156
David Maidenberg <david.maidenberg@epitech.eu>
2019-03-26 21:27:37 -0700

    update json

--- Assets/Scripts/Core/SerializationController.cs
+++ Assets/Scripts/Core/SerializationController.cs
@@ -11,6 +11,7 @@ public class SerializationController : MonoBehaviour
 
     public static SerializationController instance = null;
 
+    private GardenData.SerializedGardenData gardenData;
     private int serializationElemNb = 0;
     private List<ISerializable> items = new List<ISerializable>();
     private string json = "";
@@ -33,8 +34,14 @@ public class SerializationController : MonoBehaviour
         }
         if (Input.GetKeyDown(KeyCode.Keypad2))
             SpawnController.instance.SpawnScene(DeSerialize("{\"garden\":[{\"type\":\"FlowerBed\",\"data\":{\"name\":\"FlowerBed\",\"pointsSize\":5,\"points\":[{\"x\":46.310054779052734,\"y\":-69.38301086425781},{\"x\":48.709495544433594,\"y\":-64.33088684082031},{\"x\":53.19804000854492,\"y\":-65.67674255371094},{\"x\":50.14833068847656,\"y\":-68.9350357055664},{\"x\":46.310054779052734,\"y\":-69.38301086425781}],\"elementsSize\":3,\"elements\":[{\"subID\":0,\"position\":{\"x\":49.71393585205078,\"y\":9.5367431640625e-7,\"z\":-65.6791763305664},\"rotation\":{\"x\":0,\"y\":0,\"z\":0,\"w\":1}},{\"subID\":0,\"position\":{\"x\":49.534976959228516,\"y\":0,\"z\":-67.25696563720703},\"rotation\":{\"x\":0,\"y\":0,\"z\":0,\"w\":1}},{\"subID\":0,\"position\":{\"x\":50.65749740600586,\"y\":-9.5367431640625e-7,\"z\":-66.22111511230469},\"rotation\":{\"x\":0,\"y\":0,\"z\":0,\"w\":1}}]}},{\"type\":\"WallHandler\",\"data\":{\"start\":{\"x\":44.7247200012207,\"y\":-9.5367431640625e-7,\"z\":-67.14225769042969},\"end\":{\"x\":48.633277893066406,\"y\":-9.5367431640625e-7,\"z\":-62.90876770019531}}},{\"type\":\"WallHandler\",\"data\":{\"start\":{\"x\":50.23482894897461,\"y\":-9.5367431640625e-7,\"z\":-63.20880889892578},\"end\":{\"x\":56.12355041503906,\"y\":9.5367431640625e-7,\"z\":-64.91214752197266}}},{\"type\":\"WallHandler\",\"data\":{\"start\":{\"x\":44.066707611083984,\"y\":0,\"z\":-69.7416763305664},\"end\":{\"x\":50.33789825439453,\"y\":0,\"z\":-71.0821762084961}}},{\"type\":\"WallHandler\",\"data\":{\"start\":{\"x\":51.573123931884766,\"y\":0,\"z\":-69.9146957397461},\"end\":{\"x\":55.243289947509766,\"y\":0,\"z\":-65.70992279052734}}}]}"));
+        if (Input.GetKeyDown(KeyCode.Keypad1))
+        {
+            Vector2 tmp = new Vector2(1, 1);
+            Debug.Log(JsonUtility.ToJson(tmp));
+        }
     }
 
+    public void SetGardenData(GardenData.SerializedGardenData gardenData) { this.gardenData = gardenData; }
 
     public void AddToList(ISerializable item) { this.items.Add(item); }
 
@@ -47,7 +54,9 @@ public class SerializationController : MonoBehaviour
             ErrorHandler.instance.ErrorMessage("Nothing to save");
             return;
         }
-        this.json = "";
+        this.json = "{\"name\":\"" + gardenData.name + "\",";
+        this.json += "\"bounds\":[" + JsonUtility.ToJson(gardenData.bounds[0]);
+        this.json += ", " + JsonUtility.ToJson(gardenData.bounds[1]) + "],";
         this.serializationElemNb = 0;
         this.closed = false;
         foreach (ISerializable item in items)
@@ -103,7 +112,7 @@ public class SerializationController : MonoBehaviour
     private void AddItem(string item)
     {
         if (this.serializationElemNb == 0)
-            this.json = "{\"garden\":[" + item;
+            this.json += "\"garden\":[" + item;
         else
             this.json += "," + item;
         this.serializationElemNb++;
--- Assets/Scripts/GardenData.cs
+++ Assets/Scripts/GardenData.cs
@@ -5,17 +5,12 @@ using UnityEngine;
 
 public class GardenData : MonoBehaviour, ISerializable
 {
-    public string gardenName;
+    public string gardenName = "PlaceHolder";
     private Vector2[] bounds = new Vector2[2] { new Vector2(0, 0), new Vector2(100, 100) };
 
     private void Start()
     {
-        SerializationController.instance.AddToList(this);
-    }
-
-    private void OnDestroy()
-    {
-        SerializationController.instance.RemoveFromList(this);
+        SerializationController.instance.SetGardenData(StoreData());
     }
 
     //Serialization
@@ -26,15 +21,21 @@ public class GardenData : MonoBehaviour, ISerializable
         public Vector2[] bounds;
     }
 
+    private SerializedGardenData StoreData()
+    {
+        SerializedGardenData gardenData;
+
+        gardenData.name = gardenName;
+        gardenData.bounds = bounds;
+        return gardenData;
+    }
+
     public SerializationData Serialize()
     {
         SerializationData tmp;
-        SerializedGardenData gardenData;
 
         tmp.type = SerializationController.ItemType.GardenData;
-        gardenData.name = gardenName;
-        gardenData.bounds = bounds;
-        tmp.data = JsonUtility.ToJson(gardenData);
+        tmp.data = JsonUtility.ToJson(StoreData());
         return tmp;
     }
 
@@ -43,5 +44,6 @@ public class GardenData : MonoBehaviour, ISerializable
         SerializedGardenData gardenData = JsonUtility.FromJson<SerializedGardenData>(json);
 
         gardenName = gardenData.name;
+        SerializationController.instance.SetGardenData(StoreData());
     }
 }
