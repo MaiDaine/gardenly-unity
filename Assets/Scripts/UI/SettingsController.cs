5c592b88477b9fce24120d21ab971ed01daabf22
yan.fasquelle@epitech.eu <yan.fasquelle@epitech.eu>
2019-05-07 19:00:19 +0300

    remove useless settings + new settings menu with doozy + start flowerBed menu system

--- Assets/Scripts/UI/SettingsController.cs
+++ Assets/Scripts/UI/SettingsController.cs
@@ -5,37 +5,23 @@ using UnityEngine.UI;
 
 public class SettingsController : MonoBehaviour
 {
-    public Dropdown resolutionDropDown;
-    public Dropdown qualityDropDown;
-    public Dropdown antialiasingDropDown;   
+    public TMPro.TMP_Dropdown qualityDropDown;
+    public TMPro.TMP_Dropdown antialiasingDropDown;
 
-    private Resolution[] resolutions;
     private GameSettings gameSettings;
 
     private void OnEnable()
     {
         this.gameSettings = new GameSettings();
-        this.resolutions = Screen.resolutions;
         
-        this.resolutionDropDown.onValueChanged.AddListener(delegate { OnResolutionChange(); });
         this.qualityDropDown.onValueChanged.AddListener(delegate { OnQualityChange(); });
         this.antialiasingDropDown.onValueChanged.AddListener(delegate { OnAntialiasingChange(); });
-
-        foreach (Resolution resolution in resolutions)
-        {
-            this.resolutionDropDown.options.Add(new Dropdown.OptionData(resolution.ToString()));
-        }
     }
 
 
     public void OnAntialiasingChange()
     {
         QualitySettings.antiAliasing = this.gameSettings.antialiasing = (int)Mathf.Pow(2f, this.antialiasingDropDown.value);
-    }
-
-    public void OnResolutionChange()
-    {
-        Screen.SetResolution(this.resolutions[this.resolutionDropDown.value].width, this.resolutions[this.resolutionDropDown.value].height, Screen.fullScreen);
     }
 
     public void OnQualityChange()
