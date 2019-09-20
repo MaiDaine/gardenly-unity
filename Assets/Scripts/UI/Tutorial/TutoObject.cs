0df40ee8ef69dbe0eca92b51d423846285d1ffea
yan.fasquelle@epitech.eu <yan.fasquelle@epitech.eu>
2019-09-20 13:46:37 +0200

    Add tutoObject script

---
+++ Assets/Scripts/UI/Tutorial/TutoObject.cs
@@ -0,0 +1,35 @@
+﻿using Doozy.Engine.UI;
+using UnityEngine;
+using TMPro;
+
+public class TutoObject : MonoBehaviour
+{
+    public UIButton[] buttons;
+    public UIView[] views;
+    public TextMeshProUGUI title;
+    public string[] instructions;
+    public int progressIndex = 0;
+
+
+    public void start()
+    {
+        title.text = GetComponent<UIButton>().TextMeshProLabel.text;
+    }
+
+    public void playTutorial()
+    {
+        buttons[progressIndex].ExecuteClick();
+    }
+
+    public void previousTutorial()
+    {
+        if (progressIndex > 0)
+            --progressIndex;
+    }
+
+    public void nextTutorial()
+    {
+        if (progressIndex < buttons.Length)
+            ++progressIndex;
+    }
+}
---
+++ Assets/Scripts/UI/Tutorial/TutorialController.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using Doozy.Engine.UI;
+using UnityEngine.EventSystems;
+using TMPro;
+
+public class TutorialController : MonoBehaviour,IPointerEnterHandler, IPointerExitHandler
+{
+    public UIView[] tutoSubMenu;
+    public UIButton[] navButtons;
+    public TextMeshProUGUI title;
+    public TextMeshProUGUI body;
+
+    private bool hoverTuto = false;
+    private bool tutoActivate = false;
+    private TutoObject tutoObject = null;
+
+    private void Update()
+    {
+        /*if (tutoActivate)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (EventSystem.current.IsPointerOverGameObject() && !hoverTuto)
+                {
+                    Debug.Log("CLICK ON UI");
+                }
+            }
+        }*/
+    }
+
+    //Play Tutorial management
+    public void SetTutorial(TutoObject tutorial)
+    {
+        tutoObject = tutorial;
+        tutoObject.progressIndex = 0;
+    }
+
+    //UI management
+    public void CloseSubMenu()
+    {
+        foreach (UIView view in tutoSubMenu)
+        {
+            if (view.IsVisible)
+                view.Hide();
+        }
+    }
+
+    public void ToogleNavButtons()
+    {
+        foreach (UIButton button in navButtons)
+        {
+            button.gameObject.SetActive(!button.IsActive());
+        }
+    }
+
+    public void ToogleTutoState()
+    {
+        tutoActivate = !tutoActivate;
+    }
+
+    public bool GetTutoState()
+    {
+        return tutoActivate;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        hoverTuto = true;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        hoverTuto = false;
+    }
+}
